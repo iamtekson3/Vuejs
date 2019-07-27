97e9e58819286b0d648f5bd07078ee0b51c2cc50
Tekson <teksondada@gmail.com>
2019-07-27 09:02:31 +0545

    javascript eventListner function added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ new Vue({
     el: '#app',
     data: {
         title: 'I am dynamic title',
+        wage: 10,
         name: 'Kamal Kshetri',
         url: 'http://tekkshetri.com.np',
         classes: ["one", 'two'],
@@ -9,6 +10,9 @@ new Vue({
     method:{
         greet(time) {
             return `Hello ${this.name}, Good ${this.time}`
+        },
+        increase(amount) {
+            wage += amount
         }
     }
 })
