6946bc6abcdf4f266ec27422f441e7b140459561
Tekson <teksondada@gmail.com>
2019-07-27 08:51:07 +0545

    Data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#app',
     data: {
         title: 'I am dynamic title',
-        name: 'Kamal Kshetri'
+        name: 'Kamal Kshetri',
+        url: 'http://tekkshetri.com.np',
+        classes: ["one", 'two'],
     },
     method:{
         greet(time) {
